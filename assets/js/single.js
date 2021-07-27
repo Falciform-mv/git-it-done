ca03eedcc7eb6a979d92f15889075e915d6f479a
Mike Veneziale <falciformdesign@gmail.com>
2021-07-27 15:21:30 -0500

    links to github if more than 30 issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,6 @@
 
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -9,6 +10,11 @@ var getRepoIssues = function(repo) {
         if (response.ok) {
             response.json().then(function(data) {
                 displayIssues(data);
+
+            // check if api has paginated issues
+            if (response.headers.get("link")) {
+                displayWarning(repo);
+            }
             });
         }
         else {
@@ -16,6 +22,7 @@ var getRepoIssues = function(repo) {
         }
     });
     console.log(repo);
+
 };
 
 var displayIssues = function(issues) {
@@ -55,6 +62,18 @@ var displayIssues = function(issues) {
 
 };
 
+
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See more issues on Github";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // appends warning to container
+    limitWarningEl.appendChild(linkEl);
+};
 
 
 getRepoIssues("facebook/react");
